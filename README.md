977c0b54a48ef0e480a782e114e9626d6e68b239
Anthony Verez <netantho@nextgenlabs.net>
2013-07-27 10:25:39 -0700

    add --add-file feature

--- README.md
+++ README.md
@@ -5,11 +5,13 @@ Planned features
 ----------------
 
 * Password management using GPG over git
+	* encryption
+	* signature
 * Commands
-	* summary: Who has access to which file
-	* list <mysecretfile.gpg>: list security information about an encrypted file including permissions
-	* cat <mysecretfile.gpg>: display the content of an encrypted file
-	* decrypt <mysecretfile.gpg>: decrypt an encrypted file, wait for the user when they're finished and re-encrypt if the file was modified
-	* add-file <mail@example.com> <mysecretfile>: Encrypt a file and add it to the repo
-	* add-key <mail@example.com> <mysecretfile.gpg>: Re-encrypt a file adding a new recipient
+	* --summary: Who has access to which file
+	* --list mysecretfile.gpg: list security information about an encrypted file including permissions
+	* --cat mysecretfile.gpg: display the content of an encrypted file
+	* --decrypt mysecretfile.gpg: decrypt an encrypted file, wait for the user when they're finished and re-encrypt if the file was modified
+	* --add-file mail@example.com mysecretfile: Encrypt a file and add it to the repo
+	* --add-key mail@example.com mysecretfile.gpg: Re-encrypt a file adding a new recipient
 
--- gpgsharedpass.sh
+++ gpgsharedpass.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 
+if [ "$(uname)" == "Darwin" ]; then
+	SHRED=srm
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	SHRED=shred
+fi
+
 while test $# -gt 0; do
         case "$1" in
 		--list)
                 	if [ $# -gt 1 ]
 			then
 				gpg --status-fd 1 --list-only -vv $2
+				gpg --verify ${2%.*}.sig $2
 			else
 				echo "Error: --list <myfile.gpg>"
 			fi
 			break
 			;;
+		--summary)
+			find . -type file -name \*.gpg -print > files
+			while read line
+			do
+				echo $line
+				gpg --status-fd 1 --list-only -vv $line
+				gpg --verify ${line%.*}.sig $line
+				echo ""
+			done < files
+			$SHRED files
+			break
+			;;
+		--add-file)
+                        if [ $# -gt 2 ]
+                        then
+				gpg -r $2 --encrypt $3
+				gpg --output $3.sig -u $2 --detach-sign $3.gpg
+				git add $3.gpg $3.sig
+				git commit
+				$SHRED $3
+                        else
+                                echo "Error: --add-file <mail@example.com> <myfile>"
+                        fi
+                        break
+                        ;;
 		*)
                         echo "GPG Shared Password Manager"
 			echo "Anthony Verez (netantho) <netantho@nextgenlabs.net>"
